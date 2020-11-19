a7890c6d45d327b13b2de9384d8dd859477af317
zaimongun <romulo.k8@hotmail.com>
2020-11-18 20:04:04 -0400

    Fazendo alteracoes na View.Produto

---
+++ LojaVirtual/LojaVirtual/LojaVirtual/Api/ViewModel/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiInterpares.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Login")]
+        [Required(ErrorMessage = "Login é Obrigatório")]
+        public string Login { get; set; }
+
+        [Display(Name = "Senha")]
+        [Required(ErrorMessage = "Senha é Obrigatório")]
+        public string Password { get; set; }
+
+        [Display(Name = "Lembrar")]
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ LojaVirtual/LojaVirtual/LojaVirtual/Api/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ApiInterpares.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginApi.Models
+{
+    public class RegisterViewModel : LoginViewModel
+    {
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "Nome é Obrigatório")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Função")]
+        [Required(ErrorMessage = "Função é Obrigatório")]
+        public string RoleName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Por favor digite seu email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
