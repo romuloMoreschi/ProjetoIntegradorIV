37ae5ba1662995abd6148a804848b8ea14fae596
Elton Naoki Iwakura <eltoniwakura@tce.ms.gov.br>
2020-11-18 21:47:54 -0400

    corrigindo implementacao de imagens

--- LojaVirtual/LojaVirtual/LojaVirtual/Areas/Colaborador/Controllers/ProdutoController.cs
+++ LojaVirtual/LojaVirtual/LojaVirtual/Areas/Colaborador/Controllers/ProdutoController.cs
@@ -1,9 +1,14 @@
-﻿using System.Linq;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using LojaVirtual.Models;
 using LojaVirtual.Repository.Contract;
+using LojaVirtual.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using X.PagedList;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace LojaVirtual.Areas.Colaborador.Controllers
 {
@@ -20,7 +25,24 @@ namespace LojaVirtual.Areas.Colaborador.Controllers
 
         public IActionResult Index(int? pagina)
         {
-            var produtos = _produtoRepository.ObterTodosProdutos(pagina);
+            var produtos = _produtoRepository.ObterTodosProdutos(pagina).Select(p => new ProdutoViewModel { 
+                Id = p.Id,
+                Nome = p.Nome,
+                Descricao = p.Descricao,
+                Valor = p.Valor,
+                ImagemByte = p.Imagem
+            });;
+
+
+            foreach(var produto in produtos)
+            {
+                if(produto.ImagemByte != null)
+                {
+                    var base64 = Convert.ToBase64String(produto.ImagemByte);
+                    produto.ImagemString = String.Format("data:image/jpg;base64,{0}", base64);
+                }                
+            }
+
             return View(produtos);
         }
 
@@ -51,11 +73,34 @@ namespace LojaVirtual.Areas.Colaborador.Controllers
         }
 
         [HttpPost]
-        public ActionResult<Produto> Cadastrar(Produto produto)
+        public async Task<IActionResult> Cadastrar(ProdutoViewModel produtoVm)
         {
             if (ModelState.IsValid)
             {
-                _produtoRepository.Cadastrar(produto);
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    await produtoVm.Imagem.CopyToAsync(memoryStream);
+
+                    // Upload the file if less than 20 MB
+                    if (memoryStream.Length < 20097152)
+                    {
+                        var produto = new Produto
+                        {
+                            Id = produtoVm.Id,
+                            Nome = produtoVm.Nome,
+                            Descricao = produtoVm.Descricao,
+                            Valor = produtoVm.Valor,
+                            Imagem = memoryStream.ToArray()
+                        };
+
+                        _produtoRepository.Cadastrar(produto);
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("File", "O arquivo é grande demais.");
+                    }
+                }
 
                 TempData["MSG_S"] = "Registro salvo com sucesso!";
                 return RedirectToAction(nameof(Index));
---
+++ LojaVirtual/LojaVirtual/LojaVirtual/ViewModel/ProdutoViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.ViewModel
+{
+    public class ProdutoViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Valor { get; set; }
+
+        [Required(ErrorMessage ="Arquivo é obrigatório")]
+        public IFormFile Imagem { get; set; }
+        public byte[] ImagemByte { get; set; }
+        public string ImagemString { get; set; }
+    }
+}
