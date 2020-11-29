0ee45fa83625b4aa9617cff900b0a2b75e9360d6
zaimongun <romulo.k8@hotmail.com>
2020-11-29 12:24:40 -0400

    Testando Layout

--- LojaVirtual/LojaVirtual/LojaVirtual/Controllers/ProdutoController.cs
+++ LojaVirtual/LojaVirtual/LojaVirtual/Controllers/ProdutoController.cs
@@ -7,36 +7,39 @@ using LojaVirtual.Database;
 using LojaVirtual.Models;
 using System;
 using LojaVirtual.ViewModel;
+using LojaVirtual.Repository.Contract;
 
 namespace LojaVirtual.Controllers
 {
     public class ProdutoController : Controller
     {
         private readonly LojaVirtualContext _context;
+        readonly IProdutoRepository _produtoRepository;
 
-        public ProdutoController(LojaVirtualContext context)
+        public ProdutoController(LojaVirtualContext context, IProdutoRepository produtoRepository)
         {
             _context = context;
+            _produtoRepository = produtoRepository;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int? pagina)
         {
-            var produtos = _context.Produto.Select(p => new ProdutoViewModel
-            {
+             var produtos = _produtoRepository.ObterTodosProdutos(pagina).Select(p => new ProdutoViewModel { 
                 Id = p.Id,
                 Nome = p.Nome,
                 Descricao = p.Descricao,
                 Valor = p.Valor,
                 ImagemByte = p.Imagem
-            }); ;
+            });;
 
-            foreach (var produto in produtos)
+
+            foreach(var produto in produtos)
             {
-                if (produto.ImagemByte != null)
+                if(produto.ImagemByte != null)
                 {
                     var base64 = Convert.ToBase64String(produto.ImagemByte);
                     produto.ImagemString = String.Format("data:image/jpg;base64,{0}", base64);
-                }
+                }                
             }
 
             return View(produtos);
